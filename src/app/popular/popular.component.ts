d250462019d96cd1b5cc830e0215df3a831dc8cf
rodgc <rod.gc@hotmail.com>
2017-05-26 22:09:07 -0500

    Create get() to display contacts

--- src/app/popular/popular.component.ts
+++ src/app/popular/popular.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IContact } from "../interfaces";
+import { ContactsService } from "../contacts";
 
 @Component({
   selector: 'app-popular',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popular.component.scss']
 })
 export class PopularComponent implements OnInit {
-
-  constructor() { }
+  contacts: IContact[];
+  constructor(
+    private _contactsService: ContactsService
+  ) { }
 
   ngOnInit() {
+    this.contacts = this._contactsService.getContacts().slice(0,3);
   }
 
 }
