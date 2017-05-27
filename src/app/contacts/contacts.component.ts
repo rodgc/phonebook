dc51963119937ab83b3dadec6c165b91a9c341cc
rodgc <rod.gc@hotmail.com>
2017-05-26 22:19:00 -0500

    Display contacts list

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IContact } from "../interfaces";
+import { ContactsService } from "./contacts.service";
 
 @Component({
-  selector: 'app-contacts',
+  selector: 'contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
-
-  constructor() { }
+  contacts: IContact[];
+  constructor(
+    private _contactsService: ContactsService
+  ) { }
 
   ngOnInit() {
+    this.contacts = this._contactsService.getContacts();
   }
 
 }
