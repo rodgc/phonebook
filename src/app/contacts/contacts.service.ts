4af869dda75a8418eb372180f67f19743184e014
rodgc <rod.gc@hotmail.com>
2017-05-26 22:08:46 -0500

    Create mock services to get Contacts

--- src/app/contacts/contacts.service.ts
+++ src/app/contacts/contacts.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { IContact } from '../interfaces';
 
 @Injectable()
 export class ContactsService {
 
   constructor() { }
+
+  getContacts() {
+    // TODO Change to a http call
+    let contacts: IContact[] = [{
+      "name": "HUAN",
+      "number": "1234567899"
+    },{
+      "name": "PRUD",
+      "number": "987654321"
+    },{
+      "name": "HEINZX",
+      "number": "123987465"
+    },{
+      "name": "PRUBERT",
+      "number": "7894562584"
+    }];
+    return contacts;
+  }
 
 }
